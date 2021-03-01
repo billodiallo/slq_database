b90b8d95e47ef5f8cee6a2685374e784ba70d5a6
Billo Diallo <billodiallonet@gmail.com>
2021-03-01 08:52:22 +0300

    create files

---
+++ README.md
@@ -0,0 +1,70 @@
+# MySQL
+
+# Project: SQL Zoo
+- SQL Zoo is one of the few resources online that actually lets you build and run queries against existing tables. Each tutorial will show you a table and then have you run queries against it to answer specific questions. Some of the queries, particularly at first, are quite simple. They definitely get more challenging towards the end and can be real head scratchers.
+
+![screenshot](screenshot.png)
+
+# Getting Started
+
+- This project contains all questions and answers to them from 
+  [SQL ZOO Tutorial](https://sqlzoo.net/wiki/SELECT_from_WORLD_Tutorial)
+
+
+## Live Demo
+
+[Live Demo Link](https://repl.it/@Igors78/MySQL#README.md)
+
+## On your Computer
+
+To get a local copy up and running follow these simple example steps.
+
+### Prerequisites
+
+- Any PC or device with an internet connection and ability to use the command
+  line terminal
+
+### Setup
+
+- Open the command line terminal
+
+### Install
+
+- Go to your preferred directory
+- Run `git clone https://github.com/Igors78/MySQL.git` command
+
+### Usage
+
+- Open the cloned repo in your preferred text editor
+- Questions and answers placed in respectively named SQL files
+
+
+
+## Built with
+
+- Virtual Studio Code
+
+## Author
+
+👤 **Igors Oleinikovs**
+
+- GitHub: [@githubhandle](https://github.com/Igors78)
+- Twitter: [@twitterhandle](https://twitter.com/oleinikovs)
+- LinkedIn: [LinkedIn](https://www.linkedin.com/in/igors-oleinikovs-17a10958/)
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+- goodprogrammer.ru
+
+
+## 📝 License
+
+This project is [UNLicense](./LICENSE) licensed.
---
+++ select_from_nobel.sql
@@ -0,0 +1,171 @@
+1.
+Change the query shown so that it displays Nobel prizes for 1950.
+SELECT yr, subject, winner
+  FROM nobel
+ WHERE yr = 1950;
+
+2.
+Show who won the 1962 prize for Literature.
+
+SELECT winner
+  FROM nobel
+ WHERE yr = 1962
+   AND subject = 'Literature';
+
+3.
+Show the year and subject that won 'Albert Einstein' his prize.
+
+SELECT yr, subject
+FROM nobel
+WHERE winner = 'Albert Einstein';
+
+4.
+Give the name of the 'Peace' winners since the year 2000, including 2000.
+
+SELECT winner
+FROM nobel
+WHERE yr >= 2000
+AND subject = 'Peace';
+
+5.
+Show all details (yr, subject, winner) of the Literature prize winners for 1980 to 1989 inclusive.
+
+SELECT yr, subject, winner
+FROM nobel
+WHERE subject = 'Literature'
+AND yr >= 1980
+AND yr <= 1989;
+
+6.
+Show all details of the presidential winners:
+
+Theodore Roosevelt
+Woodrow Wilson
+Jimmy Carter
+Barack Obama
+
+SELECT * FROM nobel
+WHERE winner
+IN ('Theodore Roosevelt',
+'Woodrow Wilson',
+'Jimmy Carter',
+'Barack Obama');
+
+7.
+Show the winners with first name John
+
+SELECT winner
+FROM nobel
+WHERE winner
+LIKE 'John%';
+
+8.
+Show the year, subject, and name
+of Physics winners for 1980 together with the Chemistry winners for 1984.
+
+SELECT yr, subject, winner
+FROM nobel
+WHERE yr = 1984
+AND subject = 'Chemistry'
+UNION
+SELECT yr, subject, winner
+FROM nobel
+WHERE yr = 1980
+AND subject = 'Physics';
+
+9.
+Show the year, subject, and name of winners for 1980 excluding Chemistry and Medicine
+
+SELECT yr, subject, winner 
+FROM nobel
+WHERE yr = 1980
+AND subject NOT IN ('Chemistry',
+'Medicine');
+
+10.
+Show year, subject, and name of people who won a 'Medicine' prize in an early year
+(before 1910, not including 1910)
+together with winners of a 'Literature' prize in a later year (after 2004, including 2004)
+
+SELECT yr, subject, winner
+FROM nobel
+WHERE subject = 'Literature' AND yr >= 2004
+UNION
+SELECT yr, subject, winner
+FROM nobel
+WHERE subject = 'Medicine' AND yr < 1910;
+
+11.
+Find all details of the prize won by PETER GRÜNBERG
+
+SELECT*FROM nobel
+WHERE winner LIKE 'PETER GR_NBERG';
+
+12.
+Find all details of the prize won by EUGENE O'NEILL
+
+Escaping single quotes'
+
+SELECT*FROM nobel
+WHERE winner LIKE 'EUGENE O_NEILL';
+
+13.
+Knights in order
+
+List the winners, year and subject where the winner starts with Sir.
+Show the the most recent first, then by name order.
+
+SELECT winner, yr, subject FROM nobel
+WHERE winner LIKE 'Sir%'
+ORDER BY yr DESC;
+
+14.
+The expression subject IN ('Chemistry','Physics')
+can be used as a value - it will be 0 or 1.
+
+Show the 1984 winners and subject ordered
+by subject and winner name; but list Chemistry and Physics last.
+
+SELECT winner, subject 
+FROM nobel
+WHERE yr=1984
+ORDER BY subject IN ('Physics','Chemistry'),subject,winner;
+
+-- QUIZ 
+1. Pick the code which shows the name of winner's names beginning with C and ending in n
+SELECT winner FROM nobel
+ WHERE winner LIKE 'C%' AND winner LIKE '%n'
+'2. Select the code that shows how many Chemistry awards were given between 1950 and 1960
+SELECT COUNT(subject) FROM nobel
+ WHERE subject = 'Chemistry'
+   AND yr BETWEEN 1950 and 1960
+3. Pick the code that shows the amount of years where no Medicine awards were given
+SELECT COUNT(DISTINCT yr) FROM nobel
+ WHERE yr NOT IN (SELECT DISTINCT yr FROM nobel WHERE subject = 'Medicine')
+4. Select the result that would be obtained from the following code:
+Medicine	Sir John Eccles
+Medicine	Sir Frank Macfarlane Burnet
+5. Select the code which would show the year when neither a Physics or Chemistry award was given
+SELECT yr FROM nobel
+ WHERE yr NOT IN(SELECT yr 
+                   FROM nobel
+                 WHERE subject IN ('Chemistry','Physics'))
+6. Select the code which shows the years when a Medicine
+award was given but no Peace or Literature award was
+SELECT DISTINCT yr
+  FROM nobel
+ WHERE subject='Medicine' 
+   AND yr NOT IN(SELECT yr FROM nobel 
+                  WHERE subject='Literature')
+   AND yr NOT IN (SELECT yr FROM nobel
+                   WHERE subject='Peace')
+7. Pick the result that would be obtained from the following code:
+ SELECT subject, COUNT(subject) 
+   FROM nobel 
+  WHERE yr ='1960' 
+  GROUP BY subject
+  Chemistry	1
+Literature	1
+Medicine	2
+Peace	1
+Physics	1
